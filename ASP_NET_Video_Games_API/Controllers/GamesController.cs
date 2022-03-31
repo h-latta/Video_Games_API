e34074023c677c973eb80731faa443d20fa35cac
h-latta <h.latta@yahoo.com>
2022-03-31 15:58:39 -0500

    Created API Controller with GET and GET By Id Endpoints

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,31 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    [Route("api/games")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetGames()
+        {
+            var videoGame = _context.VideoGames.Select(p => p.Name).Distinct();
+            return Ok(videoGame);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGamesById(int id)
+        {
+            var videoGamesById = _context.VideoGames.Where(p => p.Id == id);
+            return Ok(videoGamesById);
+        }
+    }
+}
